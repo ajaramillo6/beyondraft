3470ca21cd7b74a73baa35c54ed64ac09635b0e1
Alvaro Jaramillo <alvaro_josej@hotmail.com>
2025-02-16 15:47:31 -0500

    fixed preview page and updated browserlist

--- app/(public)/(routes)/preview/[fileId]/page.tsx
+++ app/(public)/(routes)/preview/[fileId]/page.tsx
@@ -1,117 +1,109 @@
 "use client";
 
-import { useEffect, useRef, useState, Suspense, lazy } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState, Suspense, lazy, useCallback } from "react";
 import { useConvex } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import Spinner from "@/app/_components/Spinner";
 import dynamic from "next/dynamic";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useAppContext } from "@/app/context_";
 import { Id } from "@/convex/_generated/dataModel";
+import { useAuth } from "@/app/context_/AuthContext";
+import Spinner from "@/app/_components/Spinner";
+import { useWorkspace } from "@/app/context_/WorkspaceContext";
+import WorkspaceHeader from "@/app/(routes)/workspace/_components/WorkspaceHeader";
 
-const WorkspaceHeader = lazy(() =>
-  import("../../../../(routes)/workspace/_components/WorkspaceHeader")
-);
 const Editor = dynamic(
-  () => import("../../../../(routes)/workspace/_components/Editor"),
+  () => import("@/app/(routes)/workspace/_components/Editor"), 
   { suspense: true, ssr: false }
 );
 const Canvas = dynamic(
-  () => import("../../../../(routes)/workspace/_components/Canvas"),
+  () => import("@/app/(routes)/workspace/_components/Canvas"), 
   { suspense: true, ssr: false }
 );
 
-interface PreviewPageProps {
+interface WorkspacePageProps {
   params: {
     fileId: Id<"files">;
   };
 }
 
-const PreviewPage: React.FC<PreviewPageProps> = ({ params }) => {
+const PreviewPage: React.FC<WorkspacePageProps> = ({ params }) => {
   const convex = useConvex();
-  const router = useRouter();
-  const { fileData, setFileData } = useAppContext();
+
   const { mutate } = useApiMutation(api.files.updateFileName);
+
+  const { user } = useAuth();
+  const { fileData, setFileData } = useAppContext();
+  const { showEditor, showCanvas, showBoth, onSaveTrigger } = useWorkspace();
 
   const canvasRef = useRef<HTMLDivElement>(null);
   const editorRef = useRef<HTMLElement>(null);
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const isResizingRef = useRef(false);
 
-  const [triggerSave, setTriggerSave] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [value, setValue] = useState(fileData?.fileName || "Untitled");
-  const [viewState, setViewState] = useState({
-    showEditor: false,
-    showCanvas: false,
-    showBoth: true,
-  });
 
+  const fetchFileData = useCallback(async () => {
+    if (!params.fileId) return;
+    
+    const result = await convex.query(api.files.getFileById, { _id: params.fileId });
+    
+    if (JSON.stringify(result) !== JSON.stringify(fileData)) {
+      setFileData(result);
+    }
+  }, [params.fileId, convex, setFileData]);
+  
   useEffect(() => {
-    if (params.fileId) fetchFileData();
-  }, [params.fileId]);
+    fetchFileData();
+  }, [params.fileId, showEditor, showCanvas, showBoth, onSaveTrigger]);  
+  
+  const handleMouseDown = (event: React.MouseEvent) => {
+    event.preventDefault();
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
 
-  const fetchFileData = async () => {
-    try {
-      const result = await convex.query(api.files.getFileById, {
-        _id: params.fileId,
-      });
-
-      if (!result) {
-        router.push("/404");
-        return;
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    const newWidth = (event.clientX / window.innerWidth) * 100;
+    if (editorRef.current) {
+      const clampedWidth = Math.min(Math.max(newWidth, 35), 65);
+      if (editorRef.current.style.width !== `${clampedWidth}%`) {
+        editorRef.current.style.width = `${clampedWidth}%`;
       }
-
-      if (!result.isPublished) {
-        router.push("/403");
-        return;
-      }
-
-      if (result !== fileData) setFileData(result); // Only update if data is different
-    } catch (error) {
-      console.error("Error fetching file data:", error);
-      router.push("/404");
     }
   };
 
-  const handleResizeStart = (event: React.MouseEvent) => {
-    event.preventDefault();
-    isResizingRef.current = true;
-    document.addEventListener("mousemove", handleResize);
-    document.addEventListener("mouseup", handleResizeEnd);
-  };
-
-  const handleResize = (event: MouseEvent) => {
-    if (!isResizingRef.current) return;
-    const newWidth = Math.max(404, Math.min(800, event.clientX));
-    if (editorRef.current) editorRef.current.style.width = `${newWidth}px`;
-  };
-
-  const handleResizeEnd = () => {
+  const handleMouseUp = () => {
     isResizingRef.current = false;
-    document.removeEventListener("mousemove", handleResize);
-    document.removeEventListener("mouseup", handleResizeEnd);
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
   };
 
   const enableInput = () => {
     setIsEditing(true);
-    setValue(fileData?.fileName || "Untitled");
-    inputRef.current?.focus();
+    setTimeout(() => {
+      setValue(fileData?.fileName || "Untitled");
+      inputRef.current?.focus();
+    }, 0);
   };
 
   const disableInput = () => setIsEditing(false);
 
-  const handleInput = (newValue: string) => {
+  const onInput = (newValue: string) => {
+    if (newValue === value) return;
     setValue(newValue);
     mutate({
-      _id: fileData?._id,
+      _id: fileData?._id || "",
+      createdBy: user.id,
       fileName: newValue || "Untitled",
       dateModified: Date.now(),
     });
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
       disableInput();
@@ -120,66 +112,51 @@ const PreviewPage: React.FC<PreviewPageProps> = ({ params }) => {
   };
 
   return (
-    <div>
-      <WorkspaceHeader
-        preview
-        onSave={() => setTriggerSave(!triggerSave)}
-        showEditor={viewState.showEditor}
-        setShowEditor={(showEditor) =>
-          setViewState((prev) => ({ ...prev, showEditor }))
-        }
-        showCanvas={viewState.showCanvas}
-        setShowCanvas={(showCanvas) =>
-          setViewState((prev) => ({ ...prev, showCanvas }))
-        }
-        showBoth={viewState.showBoth}
-        setShowBoth={(showBoth) =>
-          setViewState((prev) => ({ ...prev, showBoth }))
-        }
-        isEditing={isEditing}
-        value={value}
-        enableInput={enableInput}
-        disableInput={disableInput}
-        onInput={handleInput}
-        onKeyDown={handleKeyDown}
-        inputRef={inputRef}
-      />
-
-      <div className="flex relative overflow-x-hidden">
-        {(viewState.showBoth || viewState.showEditor) && (
+    <>
+      <div className="sticky top-0 z-50 bg-white dark:bg-neutral-950 shadow">
+        <WorkspaceHeader
+          preview
+          isEditing={isEditing}
+          value={value}
+          enableInput={enableInput}
+          disableInput={disableInput}
+          onInput={onInput}
+          onKeyDown={onKeyDown}
+          inputRef={inputRef}
+        />
+      </div>
+      <div className="flex flex-col h-full md:flex-row md:h-screen">
+        {(showBoth || showEditor) && (
           <Suspense fallback={<Spinner />}>
             <aside
-              className="h-screen"
-              style={{ width: viewState.showBoth ? "500px" : "100%" }}
+              className={`h-full ${showBoth ? "relative" : ""}`}
+              style={{ width: showBoth ? "35%" : "100%" }}
               ref={editorRef}
             >
-              {viewState.showBoth && (
+              {showBoth && (
                 <div
-                  onMouseDown={handleResizeStart}
+                  onMouseDown={handleMouseDown}
                   className="opacity-0 hover:opacity-100 transition cursor-ew-resize 
-                  h-screen w-2 bg-zinc-200 dark:bg-zinc-800 absolute right-0 top-0 z-[99999]"
+                  h-full w-2 bg-gray-200 dark:bg-gray-800 absolute right-0 top-0 z-50 hidden md:block"
                 />
               )}
-              <Editor
+              <Editor 
                 preview
-                onSaveTrigger={triggerSave}
-                fileId={params.fileId}
+                fileId={params.fileId} 
               />
             </aside>
           </Suspense>
         )}
-
-        {(viewState.showBoth || viewState.showCanvas) && (
+        {(showBoth || showCanvas) && (
           <Suspense fallback={<Spinner />}>
             <div
-              className="h-screen border-l"
-              style={{ width: viewState.showBoth ? "calc(100% - 404px)" : "100%" }}
+              className="h-full flex-grow border-l overflow-auto"
+              style={{ width: "100%" }}
               ref={canvasRef}
             >
               {fileData && (
                 <Canvas
                   preview
-                  onSaveTrigger={triggerSave}
                   fileId={params.fileId}
                   fileData={fileData}
                 />
@@ -188,7 +165,7 @@ const PreviewPage: React.FC<PreviewPageProps> = ({ params }) => {
           </Suspense>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
